af390507faca34c779817fa950c9e14ecf1a60cb
Hulbert Alejandro <hulberta.arangof@uqvirtual.edu.co>
2025-02-17 17:01:47 -0500

    Primer commit

---
+++ src/main/java/co/edu/uniquindio/shopSystem/ShopSystemApplication.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.shopSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSystemApplication.class, args);
+    }
+}
