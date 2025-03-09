c3fba69ec37c3c9afa0cca16884f8048a29ad97e
Hulbert Alejandro <hulberta.arangof@uqvirtual.edu.co>
2025-03-09 15:33:28 -0500

    Configuración de CORS para permitir frontend

---
+++ src/main/java/co/edu/uniquindio/shopSystem/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.shopSystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Permitir solo rutas que empiezan con "/api/"
+                        .allowedOrigins("https://shop-system-frontend-822c9.web.app") // Permite tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos los headers
+                        .allowCredentials(true); // Permite credenciales (tokens, cookies, etc.)
+            }
+        };
+    }
+}
